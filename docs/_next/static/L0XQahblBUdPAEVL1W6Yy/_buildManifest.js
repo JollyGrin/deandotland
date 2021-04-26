d2e020c7572fd35d3a234b4fcc7c08fadfaf8345
JollyGrin <jollygrin@protonmail.com>
2021-04-26 15:50:57 +0200

    update

---
+++ docs/_next/static/L0XQahblBUdPAEVL1W6Yy/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-c4395f5ac203ca969ce7.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-e4077e68b11a6e9ec84d.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
